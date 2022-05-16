578e310668c720ba11737bc5c1e7366fa7bf9d2b
Sergey-McArrow <sergey.mcarrow@gmail.com>
2022-05-16 10:27:01 +0400

    feat: add slider 2022.05.16 10.26

---
+++ cssMemSlider/script.js
@@ -0,0 +1,73 @@
+console.log("Running");
+let sliderWrapper = document.querySelector(".slider-wrapper");
+let slides = document.querySelectorAll(".slide");
+let dots = document.querySelector(".dots");
+let dot = document.querySelectorAll(".dot");
+let images = document.querySelectorAll(".image");
+let counter = 1;
+console.log(images);
+
+const activeSlides = (number) => {
+//   console.log(number);
+  for (let slide of slides) {
+    slide.classList.remove("active");
+  }
+  slides[number].classList.add("active");
+};
+
+const activeDots = (number) => {
+  for (let item of dot) {
+    item.classList.remove("enabled");
+  }
+  dot[number].classList.add("enabled");
+};
+
+const call = (i) => {
+  activeSlides(i);
+  activeDots(i);
+};
+
+const moveLeft = () => {
+  images.forEach((element) => element.classList.add("transition-left"));
+  for (let i = 0; i < dots.children.length; i++) {
+    dots.children[i].removeEventListener("click", moveLeft);
+    dots.children[i].removeEventListener("click", moveRight);
+  }
+};
+const moveRight = () => {
+  images.forEach((element) => element.classList.add("transition-right"));
+  for (let i = 0; i < dots.children.length; i++) {
+    dots.children[i].removeEventListener("click", moveLeft);
+    dots.children[i].removeEventListener("click", moveRight);
+  }
+};
+
+images.forEach((img) =>
+  img.addEventListener("animationend", (animationEvent) => {
+    if (animationEvent.name === "move-left") {
+      images.forEach((element) => element.classList.remove("transition-left"));
+      console.log("remove left");
+    } else {
+      images.forEach((element) => element.classList.remove("transition-right"));
+      console.log("remove right");
+    }
+  })
+);
+
+const onClickDots = () => {
+  for (let i = 0; i < dots.children.length; i++) {
+    dots.children[i].addEventListener("click", () => {
+        if (i < counter) {
+            counter=i
+            moveRight()
+        }
+        else {
+            counter=i
+            moveLeft()
+        }
+        console.log(counter)
+      call(i);
+    });
+  }
+};
+onClickDots();
